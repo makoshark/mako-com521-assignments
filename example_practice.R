dff1a7a77391ee19c6b69f71fee047b4ae09ed1f
Benjamin Mako Hill <mako@atdot.cc>
2017-01-04 16:56:09 -0800

    deleted old file and added week 2 homework start

--- example_practice.R
+++
@@ -1,6 +0,0 @@
-ages <- c(36, 35, 4)
-names(ages) <- c("mako", "mika", "atom")
-
-length(ages)
-mean(ages)
-hist(ages)
---
+++ week_02_programming_challenges.R
@@ -0,0 +1,4 @@
+load("~/GitHub/mako-com521-assignments/week2_dataset-anissa.RData")
+
+length(week2.dataset)
+hist(week2.dataset)
